66db917cfb004606b80ba48f734e53de2e8f5196
Rodrigo Baggio <rodrigo.baggio.si@gmail.com>
2022-03-09 15:18:45 -0300

    Project Finish

--- src/modules/users/useCases/listAllUsers/ListAllUsersController.ts
+++ src/modules/users/useCases/listAllUsers/ListAllUsersController.ts
@@ -3,10 +3,19 @@ import { Request, Response } from "express";
 import { ListAllUsersUseCase } from "./ListAllUsersUseCase";
 
 class ListAllUsersController {
-  constructor(private listAllUsersUseCase: ListAllUsersUseCase) {}
+  constructor(private listAllUsersUseCase: ListAllUsersUseCase) { }
 
   handle(request: Request, response: Response): Response {
-    // Complete aqui
+    const { user_id } = request.headers;
+
+    try {
+      const all = this.listAllUsersUseCase.execute({
+        user_id: String(user_id),
+      });
+      return response.status(201).json(all);
+    } catch (error) {
+      return response.status(400).send({ error });
+    }
   }
 }
 
